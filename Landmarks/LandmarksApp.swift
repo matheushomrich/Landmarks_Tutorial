030ebd669975ab7c1a46601bce70b9ce030c4943
Matheus Homrich <matidasilva34@gmail.com>
2021-03-24 15:06:49 -0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Matheus Homrich on 24/03/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
