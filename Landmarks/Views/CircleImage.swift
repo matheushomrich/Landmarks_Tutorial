05d64edf3190ea985b28724cede5ed84bc1754fb
Matheus Homrich <matidasilva34@gmail.com>
2021-03-24 17:52:00 -0300

    first commit

---
+++ Landmarks/Views/CircleImage.swift
@@ -0,0 +1,27 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Matheus Homrich on 24/03/21.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var image: Image
+
+    var body: some View {
+        image
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage(image: Image("turtlerock"))
+    }
+}
+
+
